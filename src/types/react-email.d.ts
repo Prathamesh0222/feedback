bb44ecb8af1aff826d0330c4d20cedf60168d6be
prathameshpimpalkar <prathameshpimpalkar2002@gmail.com>
2024-08-28 20:51:20 +0530

    feat: Add VerificationEmail component and sendVerificationEmail helper function

---
+++ src/types/react-email.d.ts
@@ -0,0 +1,10 @@
+declare module 'react-email/components' {
+    export const Html: React.FC<any>;
+    export const Head: React.FC<any>;
+    export const Font: React.FC<any>;
+    export const Preview: React.FC<any>;
+    export const Section: React.FC<any>;
+    export const Row: React.FC<any>;
+    export const Heading: React.FC<any>;
+    export const Text: React.FC<any>;
+}
