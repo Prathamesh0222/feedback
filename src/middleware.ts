f264062dfec2338c29953e625c1c350b24aa2230
prathameshpimpalkar <prathameshpimpalkar2002@gmail.com>
2024-08-29 23:42:25 +0530

    feat: Add .env.example file and middleware module

---
+++ src/middleware.ts
@@ -0,0 +1,27 @@
+import { NextResponse, NextRequest } from 'next/server'
+export { default } from "next-auth/middleware"
+import { getToken } from "next-auth/jwt"
+
+export async function middleware(request: NextRequest) {
+    const token = await getToken({ req: request })
+    const url = request.nextUrl
+
+    if (token &&
+        (url.pathname.startsWith("/signin")) ||
+        (url.pathname.startsWith("/signup")) ||
+        (url.pathname.startsWith("/verify")) ||
+        (url.pathname.startsWith("/dashboard"))
+    ) {
+        return NextResponse.redirect(new URL('/dashboard', request.url))
+    }
+}
+
+export const config = {
+    matcher: [
+        '/signin',
+        '/signup',
+        '/',
+        '/dashboard/:path*',
+        '/verify'
+    ]
+}
